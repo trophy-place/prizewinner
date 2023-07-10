75c1435b3b0e127d8a717365012f05d251cbf8d5
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2023-07-10 19:17:22 -0300

    feat: created Authentication Manager singleton

--- services/AuthenticationManager.ts
+++ services/AuthenticationManager.ts
@@ -1,4 +1,5 @@
 // Data
+import { authenticationManagerNotInitializedErrorMessage } from "../data/authentication/authenticationManagerNotInitializedErrorMessage.ts";
 import { expiredRefreshTokenErrorMessage } from "../data/authentication/expiredRefreshTokenErrorMessage.ts";
 //
 import { refreshAuthorizationToken } from "../src/authentication/refreshAuthorizationToken.ts";
@@ -8,7 +9,7 @@ import type { AuthenticationData } from "../types/authentication/AuthenticationD
 import { nowTimestamp } from "./nowTimestamp.ts";
 
 /**
- * Automatically manages your authentication status for you, if initialized. To initialize, don't pass `true` as the second parameter of neither `authenticateWithNpsso()` nor `refreshAuthorizationToken()`.
+ * Automatically manages your authentication status for you, if initialized. To initialize, don't pass `true` as the second parameter of `authenticateWithNpsso()`.
  *
  * If initialize, `AuthenticationManager` will be used by all functions to automatically refresh your expired `accessToken` whenever you make a request for data.
  *
@@ -18,8 +19,10 @@ class AuthenticationManager {
   #initialized = false;
   #accessToken = "";
   #tokenExpirationEpoch = 0;
+  #humanReadableTokenExpiration = "";
   #refreshToken = "";
   #refreshTokenExpirationEpoch = 0;
+  #humanReadableRefreshTokenExpiration = "";
 
   /**
    * Gets a valid `accessToken` if you haven't previously disabled the initialization of the `AuthenticationManager`. Throws an error if you never initialized the `AuthenticationManager` or if your `refreshToken` is too old to be refreshed.
@@ -28,9 +31,7 @@ class AuthenticationManager {
    */
   async getAccessCode() {
     if (this.#initialized === false) {
-      throw new Error(
-        "Cannot return token because you have never initialized the AuthenticationManager. Please run 'authenticateWithNpsso()' and don't pass the second parameter as 'true' to initialize the AuthenticationManager if you want your authentication to be automatically managed for you.",
-      );
+      throw new Error(authenticationManagerNotInitializedErrorMessage);
     }
 
     const now = nowTimestamp();
@@ -45,15 +46,40 @@ class AuthenticationManager {
   }
 
   /**
+   * Retrieve the entire Authentication token.
+   *
+   * @returns A `AuthenticationData` object returned by either `refreshAuthorizationToken()` or `authenticateWithNpsso()`.
+   */
+  getFullToken() {
+    if (this.#initialized === false) {
+      throw new Error(authenticationManagerNotInitializedErrorMessage);
+    }
+
+    const token: AuthenticationData = {
+      accessToken: this.#accessToken,
+      tokenExpirationEpoch: this.#tokenExpirationEpoch,
+      humanReadableTokenExpiration: this.#humanReadableTokenExpiration,
+      refreshToken: this.#refreshToken,
+      refreshTokenExpirationEpoch: this.#refreshTokenExpirationEpoch,
+      humanReadableRefreshTokenExpiration:
+        this.#humanReadableRefreshTokenExpiration,
+    };
+    return token;
+  }
+
+  /**
    * Updates the `AuthenticationManager`'s token properties.
    *
-   * @param token An `AuthenticationData` object returned by either `refreshAuthorizationToken()` or `authenticateWithNpsso()`.
+   * @param token A `AuthenticationData` object returned by either `refreshAuthorizationToken()` or `authenticateWithNpsso()`.
    */
   #setToken(token: AuthenticationData) {
     this.#accessToken = token.accessToken;
     this.#tokenExpirationEpoch = token.tokenExpirationEpoch;
+    this.#humanReadableTokenExpiration = token.humanReadableTokenExpiration;
     this.#refreshToken = token.refreshToken;
     this.#refreshTokenExpirationEpoch = token.refreshTokenExpirationEpoch;
+    this.#humanReadableRefreshTokenExpiration =
+      token.humanReadableRefreshTokenExpiration;
   }
 
   /**
@@ -72,7 +98,7 @@ class AuthenticationManager {
   /**
    * Initialize this instance of `AuthenticationManager`.
    *
-   * @param token An `AuthenticationData` object returned by either `refreshAuthorizationToken()` or `authenticateWithNpsso()`.
+   * @param token A `AuthenticationData` object returned by either `refreshAuthorizationToken()` or `authenticateWithNpsso()`.
    */
   initializeToken(token: AuthenticationData) {
     this.#setToken(token);
